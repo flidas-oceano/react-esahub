0ff6349658a684d331b194d8fbadd47c268cfe84
Gomez Tomas Gonzalo <tomasgomez@oceano.com.ar>
2022-08-08 18:33:10 -0300

    Modify UI

--- src/routes.js
+++ src/routes.js
@@ -16,13 +16,13 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import Dashboard from "views/Dashboard.js";
+/* import Dashboard from "views/Dashboard.js";
 import Notifications from "views/Notifications.js";
 import Icons from "views/Icons.js";
 import Typography from "views/Typography.js";
 import TableList from "views/Tables.js";
 import Maps from "views/Map.js";
-import UserPage from "views/User.js";
+import UserPage from "views/User.js"; */
 /* import UpgradeToPro from "views/Upgrade.js"; */
 import EsahubSales from "views/EsahubSales.js";
 
--- src/views/EsahubSales.js
+++ src/views/EsahubSales.js
@@ -1,16 +1,13 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 // reactstrap components
 import {
   Card,
-  CardHeader,
   CardBody,
   CardFooter,
   CardTitle,
   Row,
   Col,
   FormGroup,
-  Label,
-  Input,
   Button,
 } from "reactstrap";
 
@@ -20,28 +17,29 @@ import Swal from "sweetalert2";
 import SimpleLoading from "components/Loading/SimpleLoading";
 
 function EsahubSales() {
-  const selectRef = useRef();
-  const [option, setOption] = useState(null);
+
   const [preview, setPreview] = useState("");
   const [hasRequest, setHasRequest] = useState(false);
 
   useEffect(() => {
-    moment.locale("es");
-    console.log({option, selectRef, preview , hasRequest});
-  }, [option, hasRequest]);
+    moment.locale("es",{
+        months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
+        monthsShort: 'Enero._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.'.split('_'),
+        weekdays: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado'.split('_'),
+        weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
+        weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_')
+      });
 
-  const handleOption = (evt) => {
-    let transformOption = moment(evt.target.value).format("YYYYMMDD");
-    console.log({ transformOption });
-    setOption(evt.target.value);
-    setPreview(evt.target.value);
-  };
+    setPreview(moment().format("YYYY/MM/01"))
+  }, [hasRequest]);
+
 
   const handleLoding = (status) =>{
     setHasRequest(status);
   }
 
   const handleFetch = (evt, fromPeriod) => {
+    console.log({fromPeriod})
     handleLoding(true)
     const url = `https://www.oceanomedicina.net/laravel-foclis/api/esahub/ventas-por-pedido${
       fromPeriod && `?period=${fromPeriod}`
@@ -49,11 +47,8 @@ function EsahubSales() {
 
     axios.get(url)
       .then((res) => {
-        console.log({ data: res.data, selectRef });
+        console.log({ data: res.data });
         handleToast("Completado!", "success")
-        selectRef.current.value = null
-        setOption(null);
-        setPreview("");
         handleLoding(false)
 
       }).catch((err) => {
@@ -91,6 +86,7 @@ function EsahubSales() {
               Esta aplicacion se conecta a la API de Laravel Foclis consultando
               los registros mediante un periodo de tiempo mensual.
             </p>
+            <p>Por defecto solo puede solicitar datos en el mes actual</p>
           </Col>
           <Col lg="12" md="12" sm="12">
             <FormGroup>
@@ -98,60 +94,12 @@ function EsahubSales() {
                 <CardBody>
                   <Row>
                     <Col md="4" xs="5">
-                      <Label for="monthSelect">
-                        Seleccione el periodo mensual
-                      </Label>
-                      <Input
-                        ref={selectRef}
-                        type="select"
-                        name="select"
-                        id="monthSelect"
-                        onChange={handleOption}
-                      >
-                        <option defaultValue>Seleccione una opcion</option>
-                        <option value={moment("1/1/22").format("L")}>
-                          Enero
-                        </option>
-                        <option value={moment("2/1/22").format("L")}>
-                          Febrero
-                        </option>
-                        <option value={moment("3/1/22").format("L")}>
-                          Marzo
-                        </option>
-                        <option value={moment("4/1/22").format("L")}>
-                          Abril
-                        </option>
-                        <option value={moment("5/1/22").format("L")}>
-                          Mayo
-                        </option>
-                        <option value={moment("6/1/22").format("L")}>
-                          Junio
-                        </option>
-                        <option value={moment("7/1/22").format("L")}>
-                          Julio
-                        </option>
-                        <option value={moment("8/1/22").format("L")}>
-                          Agosto
-                        </option>
-                        <option value={moment("9/1/22").format("L")}>
-                          Septiembre
-                        </option>
-                        <option value={moment("10/1/22").format("L")}>
-                          Octubre
-                        </option>
-                        <option value={moment("11/1/22").format("L")}>
-                          Noviembre
-                        </option>
-                        <option value={moment("12/1/22").format("L")}>
-                          Diciembre
-                        </option>
-                      </Input>
+                      
                     </Col>
-
-                    {preview !== "" && (
+                    
                       <Col md="8" xs="7">
                         <div className="numbers">
-                          <p className="card-category">Periodo</p>
+                          <p className="card-category">Periodo ({moment().format("MMMM")})</p>
                           <CardTitle tag="p">
                             {moment(preview).format("DD/MM/YYYY")} hasta{" "}
                             {moment(preview)
@@ -161,20 +109,20 @@ function EsahubSales() {
                           </CardTitle>
                         </div>
                       </Col>
-                    )}
+                    
                   </Row>
                 </CardBody>
                 <CardFooter>
                   <hr />
 
-{ hasRequest ? <SimpleLoading /> : <Button
-                    color="success"
-                    style={{ marginLeft: "auto", display: "block" }}
-                    disabled={option === null || hasRequest ? true : false}
-                    onClick={(evt) => handleFetch(evt, option)}
-                  >
-                    Solicitar
-                  </Button>}
+            { hasRequest ? <SimpleLoading /> : <Button
+                                                  color="success"
+                                                  style={{ marginLeft: "auto", display: "block" }}
+                                                  disabled={ hasRequest ? true : false}
+                                                  onClick={(evt) => handleFetch(evt, moment(preview).format("YYYYMMDD"))}
+                                                >
+                                                  Solicitar
+                                                </Button>}
                   
                 </CardFooter>
               </Card>
